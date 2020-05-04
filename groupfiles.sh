609a8464b7ae1e72aa08500103785b7248a0f38f
Chon Kou <k.kou@alumni.ubc.ca>
2020-05-03 18:11:41 -0700

    Fix typos

--- groupfiles.sh
+++ groupfiles.sh
@@ -4,16 +4,16 @@
 # This script is used with find command to move files into subdirectories
 # based on their file names.
 
-# Assume file names start with year and month (YYYYMM) like this "202005"
+# Assume file names start with year and month (YYYYMM) like this "20200530.jpg"
 
 # Usage: 1. cd to the directory where the files to group are located.
 #        2. find . -iname '*?.?*' -type f -exec /path/to/this/script.sh {} +
-# Note that '*?.?*' is for avoid matching files like .bashrcc and blah.
+# Note that '*?.?*' is for avoid matching files like .bashrc and blah.
 
 for i
 do
     FILENAME="${i##*/}"
-    YEAR="${FILENAME:0:4}" #first 4 chars
+    YEAR="${FILENAME:0:4}" # first 4 chars
     MONTH="${FILENAME:4:2}" # chars 5 to 6
     NEWDIR="$YEAR/$MONTH"
     NEWDIRPATH="$PWD/$NEWDIR"
